271f4ce777b14c287001eecb94015590af6faf55
Rob Brunett <rb3@Robs-Air.attlocal.net>
2021-06-02 20:27:58 -0400

    employee type question creation

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 //modules
 const inquirer = require("inquirer");
 const fs = require("fs");
-
+//required files
+const Engineer = require('./src/engineer');
+const Intern = require('./src/intern');
+const Manager = require('./src/manager');
+//array to store created employees
+const employeeData = [];
+//Employee type selection
+const employeeType = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: "What is the Employee's role?",
+            name: 'role',
+            choices: [Manager, Engineer, Intern]
+        },
+    ])
+    .then(answer => {
+        if (answer.role === 'Manager'){
+            mgmtInput();
+        }
+        else if (answer.role === 'Engineer'){
+            engInput();
+        }
+        else if (answer.role === 'Intern'){
+            intInput();
+        }
+    })
+}
+employeeType();
