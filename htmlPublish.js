921a6c240958764550285bed7ca073fb3c0cfe94
Rob Brunett <rb3@Robs-Air.attlocal.net>
2021-06-07 19:25:45 -0400

    another try

--- htmlPublish.js
+++ htmlPublish.js
@@ -8,58 +8,58 @@ const render = employees => {
 
     html.push(employees
         .filter(employee => employee.getRole() === "Manager")
-        .map(manager => renderManager(manager))
+        .map(manager => pubMgr(manager))
     );
     html.push(employees
         .filter(employee => employee.getRole() === "Engineer")
-        .map(engineer => renderEngineer(engineer))
+        .map(engineer => pubEng(engineer))
     );
     html.push(employees
         .filter(employee => employee.getRole() === "Intern")
-        .map(intern => renderIntern(intern))
+        .map(intern => pubInt(intern))
     );
 
-    return renderMain(html.join(""));
+    return pubIndex(html.join(""));
 
 };
 
-const renderManager = manager => {
-    let output = fs.readFileSync(path.resolve(distroDir, "manager.html"), "utf8");
-    output = replacePlaceholders(output, "name", manager.getName());
-    output = replacePlaceholders(output, "role", manager.getRole());
-    output = replacePlaceholders(output, "email", manager.getEmail());
-    output = replacePlaceholders(output, "id", manager.getId());
-    output = replacePlaceholders(output, "officeNumber", manager.getOfficeNumber());
+const pubMgr = manager => {
+    let output = fs.readFileSync(path.resolve(distroDir, "mgr.html"), "utf8");
+    output = temp(output, "name", manager.getName());
+    output = temp(output, "role", manager.getRole());
+    output = temp(output, "email", manager.getEmail());
+    output = temp(output, "id", manager.getId());
+    output = temp(output, "officeNumber", manager.getOfficeNumber());
     return output;
 };
 
-const renderEngineer = engineer => {
-    let output = fs.readFileSync(path.resolve(distroDir, "engineer.html"), "utf8");
-    output = replacePlaceholders(output, "name", engineer.getName());
-    output = replacePlaceholders(output, "role", engineer.getRole());
-    output = replacePlaceholders(output, "email", engineer.getEmail());
-    output = replacePlaceholders(output, "id", engineer.getId());
-    output = replacePlaceholders(output, "github", engineer.getGithub());
+const pubEng = engineer => {
+    let output = fs.readFileSync(path.resolve(distroDir, "eng.html"), "utf8");
+    output = temp(output, "name", engineer.getName());
+    output = temp(output, "role", engineer.getRole());
+    output = temp(output, "email", engineer.getEmail());
+    output = temp(output, "id", engineer.getId());
+    output = temp(output, "github", engineer.getGithub());
     return output;
 };
 
-const renderIntern = intern => {
+const pubInt = intern => {
     let output = fs.readFileSync(path.resolve(distroDir, "intern.html"), "utf8");
-    output = replacePlaceholders(output, "name", intern.getName());
-    output = replacePlaceholders(output, "role", intern.getRole());
-    output = replacePlaceholders(output, "email", intern.getEmail());
-    output = replacePlaceholders(output, "id", intern.getId());
-    output = replacePlaceholders(output, "school", intern.getSchool());
+    output = temp(output, "name", intern.getName());
+    output = temp(output, "role", intern.getRole());
+    output = temp(output, "email", intern.getEmail());
+    output = temp(output, "id", intern.getId());
+    output = temp(output, "school", intern.getSchool());
     return output;
 };
 
-const renderMain = html => {
-    const output = fs.readFileSync(path.resolve(distroDir, "main.html"), "utf8");
-    return replacePlaceholders(output, "team", html);
+const pubIndex = html => {
+    const output = fs.readFileSync(path.resolve(distroDir, "index.html"), "utf8");
+    return temp(output, "team", html);
 };
 
-const replacePlaceholders = (output, placeholder, value) => {
-    const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
+const temp = (output, holder, value) => {
+    const pattern = new RegExp("{{ " + holder + " }}", "gm");
     return output.replace(pattern, value);
 };
 
